6c652e27d7aa214b130ea20ad8f4b683b5155ec9
Hongaproject <tkwlscjq99@gmail.com>
2024-06-10 23:40:22 +0900

    프로그래머스 3일차 코드 작성 중

--- programmers/기초/3day.js
+++ programmers/기초/3day.js
@@ -12,3 +12,28 @@ return answer
 // 코드 해석
 // 길이가 같은 두 문자열 str1, str2의 길이는 같음 그니까 for문을 1개만 돌려도 된다는 소리.
 // for문이 돌면서 1번당 answer값에는 ab가 들어가게 된다. 
+
+// 문자 리스트를 문자열로 변환하기
+// 문자들이 담겨있는 배열 arr가 주어지고 arr원소들을 순서대로 이어 붙인 문자열을 return하는 코드를 작성.
+// 입출력 예
+// arr	result
+// ["a","b","c"]	"abc"
+arr.join("");
+
+// join -> 배열의 모든 요소를 연결해 하나의 문자열로 만듭니다.
+// join(); 생략을 하면 자동으로 ,로 구분을 해줍니다. 
+// join(""); 배열의 ""  
+
+// 문자열 곱하기
+// 문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+// 입출력 예
+// my_string	k	result
+// "string"	    3	"stringstringstring"
+// "love"	    10	"lovelovelovelovelovelovelovelovelovelove"
+let answer1 = "";
+
+for(let i = 0; i<k; i++){
+    answer1 += my_string
+}
+
+return answer1;
