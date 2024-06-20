acab0165e405957c3b5b00e8ca844ab9f0850b81
Hongaproject <tkwlscjq99@gmail.com>
2024-06-20 11:44:52 +0900

    프로그래머스 5일차 기초

--- programmers/기초/5day.js
+++ programmers/기초/5day.js
@@ -87,5 +87,14 @@ function solution(num_list) {
     return 0;
 }
 
+// 이어 붙인 수
+// 정수가 담긴 리스트 num_list가 주어집니다. num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+    let sol = num_list.filter(el=>(el%2===1));
+    let sol2 = num_list.filter(el=>(el%2===0));
+    return Number(sol.join(''))+Number(sol2.join(''));
+}
+
 
 
