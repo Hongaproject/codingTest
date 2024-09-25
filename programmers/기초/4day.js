7c5264e33421b3c71b94face69f63280297e0f1b
Hongaproject <tkwlscjq99@gmail.com>
2024-09-25 23:50:13 +0900

    프로그래머스 기초4

--- programmers/기초/4day.js
+++ programmers/기초/4day.js
@@ -25,6 +25,10 @@ if(number % n === 0 && number % m === 0){
     return 0
 }
 
+function solution(number, n, m) {
+    var answer = number % n === 0 && number % m === 0 ? 1 : 0;
+    return answer;
+}
 // 홀짝에 따라 다른 값 변환하기
 // 양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고 n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
 // 입출력 예 #1
