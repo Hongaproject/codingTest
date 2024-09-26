36e5461e8472f018e0f607b5af95e8e3505e7240
Hongaproject <tkwlscjq99@gmail.com>
2024-09-26 23:50:17 +0900

    프로그래머스 기초5

--- programmers/기초/4day.js
+++ programmers/기초/4day.js
@@ -71,6 +71,43 @@ function solution(ineq, eq, n, m) {
     return 0
 }
 
+function solution(ineq, eq, n, m) {
+
+    let result = 0;
+
+    if(eq === "=") {
+        if(ineq === ">") {
+            if(n >= m) {
+                result = 1;
+            } else {
+                result = 0;
+            }
+        } else if(ineq === "<") {
+            if(n <= m) {
+                result = 1;
+            } else {
+                result = 0;
+            }
+        }
+    } else if(eq === "!") {
+        if(ineq === ">") {
+            if(n > m) {
+                result = 1;
+            } else {
+                result = 0;
+            }
+        } else if(ineq === "<") {
+            if(n < m) {
+                result = 1;
+            } else {
+                result = 0;
+            }
+        }
+    }
+    return result;
+}
+
+
 // flag에 따라 다른 값 반환하기
 // 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
 // 입출력 예 #1
@@ -83,5 +120,13 @@ function solution(a, b, flag) {
         return a + b
     } else {
         return a - b
+    }
+}
+
+function solution(a, b, flag) {
+    if(flag === true){
+        return a + b;
+    } else {
+        return a - b;
     }
 }
--- programmers/기초/5day.js
+++ programmers/기초/5day.js
@@ -19,12 +19,26 @@ function solution(code) {
     
     for (let i=0; i<code.length; i++){
         if(mode == 0){
-        code[i] == '1' ? (mode=1):(i%2==0&&(ret+=code[i]))
+            code[i] == '1' ? (mode=1):(i%2==0&&(ret+=code[i]))
         }else{
             code[i] =='1' ? (mode=0):(i%2!==0&&(ret+=code[i]))
         }
     }
-    return ret.length > 0 ? ret : "EMPTY";
+    return ret.length > 0 ? ret : "EMPTY"; // >를 통해서 ret가 비어있는지 검사
+}
+
+function solution(code) {
+    var ret = '';
+    let mode = 0;
+    
+    for(let i = 0; i<code.length; i++){
+        if(mode==0){
+            code[i] == '1' ? (mode=1) : (i%2==0&&(ret+=code[i]))
+        }else{
+            code[i] == '1' ? (mode=0) : (i%2!==0&&(ret+=code[i]))
+        }
+    }
+    return ret.length===0 ? "EMPTY" : ret; // ===를 통해서 ret가 비어있는지 검사
 }
 
 // 등차수열의 특정한 항만 더하기
@@ -42,6 +56,17 @@ function solution(code) {
 // 등차수열	3	7	11	15	19
 // included	true	false	false	true	true
 // 따라서 true에 해당하는 1항, 4항, 5항을 더한 3 + 15 + 19 = 37을 return 합니다.
+
+function solution(a, d, included) {
+    var answer = 0;
+    for(let i = 0; i<included.length; i++){ // 등차수열 항 직접 계산 그담에 sum에 더함.
+        const q = a + i * d; // 등차수열 생성 -> n번째 항 = a + (n−1) × d
+        if(included[i]){ // included[i]가 true인 경우  
+            answer += q; // answer에 등차수열 값을 누적하여 더 합니다.
+        }
+    }
+    return answer; // 값 나타내기
+}
 
 function solution(a, d, included) {
     let diffArray = []
@@ -74,26 +99,67 @@ function solution(a, b, c) {
 // 정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
 
 function solution(num_list) {
-    let add = 0;
-    let mul = 1;
-    for(let i = 0; i < num_list.length; i++){
-        mul *= num_list[i];
+    let add = 0; // 합 초기값 0
+    let mul = 1; // 곱 초기값 1
+    for(let i = 0; i<num_list.length; i++){
         add += num_list[i];
+        mul *= num_list[i];
     }
     add = add*add;
-    if(mul < add){
+    if(add > mul){ // 곱이 합의 제곱보다 작으면 1
         return 1;
-    } 
+    }
     return 0;
+}
+
+function solution(num_list) {
+    // 1. 모든 요소의 합을 구함
+    let add = num_list.reduce((a, c) => a + c, 0);
+    
+    // 2. 모든 요소의 곱을 구함
+    let mul = num_list.reduce((a, c) => a * c, 1);
+    
+    // 3. 합의 제곱과 곱을 비교하여 결과 반환
+    return (add * add > mul) ? 1 : 0;
 }
 
 // 이어 붙인 수
 // 정수가 담긴 리스트 num_list가 주어집니다. num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
 
 function solution(num_list) {
-    let sol = num_list.filter(el=>(el%2===1));
-    let sol2 = num_list.filter(el=>(el%2===0));
-    return Number(sol.join(''))+Number(sol2.join(''));
+    let sol = num_list.filter(el => (el % 2 === 1));  // 홀수만 필터링
+    let sol2 = num_list.filter(el => (el % 2 === 0)); // 짝수만 필터링
+
+    return Number(sol.join('')) + Number(sol2.join(''));  // 이어 붙여서 숫자로 변환 후 합산
+    // sol.join(''): 홀수 배열을 문자열로 이어 붙임. 예를 들어 [3, 5, 1] → '351'.
+    // sol2.join(''): 짝수 배열을 문자열로 이어 붙임. 예를 들어 [4, 2] → '42'.
+    // 이렇게 만든 문자열을 Number()로 변환하여 숫자로 만들고, 두 수의 합을 계산하는 것이 이 코드의 동작 방식입니다.
+}
+
+function solution(num_list){
+    let odd = '';
+    let even = '';
+    num_list.forEach((num) => {
+        if(num%2===0){
+            even += num;
+        }else {
+            odd += num;
+        }
+    })
+    return Number(odd)+Number(even);
+}
+
+function solution(num_list){
+    let odd = '';
+    let even = '';
+    for(let i = 0; i<num_list.length; i++){
+        if(num_list[i]%2===0){
+            even += num_list[i];
+        }else{
+            odd += num_list[i];
+        }
+    }
+    return Number(odd)+Number(even);
 }
 
 
